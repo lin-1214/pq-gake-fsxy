1b7f6726368d770942db9b0e070917adc3905111
José Ignacio Escribano <jiep12345@hotmail.com>
2021-10-29 20:22:23 +0200

    Install dependencies

--- .github/liboqs.sh
+++ .github/liboqs.sh
@@ -1,5 +1,14 @@
 ENABLED_ALGS=OQS_ENABLE_KEM_kyber_1024
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo apt update
+  sudo apt install -y ninja-build xz-utils build-essential cmake libssl-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install ninja gnu-tar xz cmake openssl@1.1
+else
+  exit 0
+fi
+
 git clone https://github.com/jiep/liboqs
 cd liboqs
 git checkout coins
