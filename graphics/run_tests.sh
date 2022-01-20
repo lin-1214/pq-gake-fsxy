4764350a6a672958e80bd35e5f62d7703bf67180
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-01-20 20:49:14 +0100

    [skip ci] Update run_tests.sh

--- graphics/run_tests.sh
+++ graphics/run_tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-./bin/test_speed_kem > ../kem.txt
-./bin/test_speed_ake > ../ake.txt
-./bin/test_speed_commitment > ../commitment.txt
+./bin/test_speed_kem > ../../kem.txt
+./bin/test_speed_ake > ../../ake.txt
+./bin/test_speed_commitment > ../../commitment.txt
 
 Ns=(512 256 128 64 32 16 8 4 2)
 for N in "${Ns[@]}"
 do
-  ./bin/test_speed_gake ${N} > ../gake_${N}.txt
+  ./bin/test_speed_gake ${N} > ../../gake_${N}.txt
 done
