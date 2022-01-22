2c50f48c4cd1b0493bf1051156494de8223f5a74
José Ignacio Escribano <jiep12345@hotmail.com>
2022-01-22 11:31:06 +0100

    Update workflow

--- graphics/convert_tables.sh
+++ graphics/convert_tables.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-FOLDER="../build"
+PARTIES_CONF="graphics/PARTIES.conf"
+JSON2CSV_FOLDER="graphics"
+FOLDER="./build"
 OUTPUT="results"
+
+reverse() {
+    # first argument is the array to reverse
+    # second is the output array
+    declare -n arr="$1" rev="$2"
+    for i in "${arr[@]}"
+    do
+        rev=("$i" "${rev[@]}")
+    done
+}
+
+IFS=$'\n' read -d '' -r -a Ns < ${PARTIES_CONF}
+reverse Ns Ns2
 
 TYPES=(
   "ake"
@@ -9,23 +24,22 @@ TYPES=(
   "commitment"
   "gake"
 )
-Ns=(512 256 128 64 32 16 8 4 2)
 
 mkdir -p ${FOLDER}/${OUTPUT}
 
 for type in "${TYPES[@]}"
 do
   if [[ ${type} == "gake" ]]; then
-    for N in "${Ns[@]}"
+    for N in "${Ns2[@]}"
     do
       type_gake="${type}_${N}"
       echo $type_gake
-      cat ${FOLDER}/${type_gake}.txt | sed '1,14d;$d' | ${FOLDER}/node_modules/parse-markdown-table-cli/bin | node json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type_gake}.csv
+      cat ${FOLDER}/${type_gake}.txt | sed '1,14d;$d' | ${FOLDER}/node_modules/parse-markdown-table-cli/bin | node ${JSON2CSV_FOLDER}/json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type_gake}.csv
       cp ${FOLDER}/${type_gake}.txt ${FOLDER}/${OUTPUT}
     done
   else
     echo $type
-    cat ${FOLDER}/${type}.txt | sed '1,14d;$d' | ${FOLDER}/node_modules/parse-markdown-table-cli/bin | node json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type}.csv
+    cat ${FOLDER}/${type}.txt | sed '1,14d;$d' | ${FOLDER}/node_modules/parse-markdown-table-cli/bin | node ${JSON2CSV_FOLDER}/json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type}.csv
     cp ${FOLDER}/${type}.txt ${FOLDER}/${OUTPUT}
   fi
 done
--- graphics/run_tests.sh
+++ graphics/run_tests.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
-./bin/test_speed_kem > ../../kem.txt
-./bin/test_speed_ake > ../../ake.txt
-./bin/test_speed_commitment > ../../commitment.txt
+PARTIES_CONF="graphics/PARTIES.conf"
+INPUT_FOLDER="./build/bin"
+OUTPUT_FOLDER="./build"
 
-Ns=(512 256 128 64 32 16 8 4 2)
-for N in "${Ns[@]}"
+reverse() {
+    # first argument is the array to reverse
+    # second is the output array
+    declare -n arr="$1" rev="$2"
+    for i in "${arr[@]}"
+    do
+        rev=("$i" "${rev[@]}")
+    done
+}
+
+IFS=$'\n' read -d '' -r -a Ns < ${PARTIES_CONF}
+reverse Ns Ns2
+
+echo -e "Running KEM..."
+${INPUT_FOLDER}/test_speed_kem > ${OUTPUT_FOLDER}/kem.txt
+
+echo -e "Running AKE..."
+${INPUT_FOLDER}/test_speed_ake > ${OUTPUT_FOLDER}/ake.txt
+
+echo -e "Running Commitment..."
+${INPUT_FOLDER}/test_speed_commitment > ${OUTPUT_FOLDER}/commitment.txt
+
+for N in "${Ns2[@]}"
 do
-  ./bin/test_speed_gake ${N} > ../../gake_${N}.txt
+  echo -e "Running GAKE-${N}..."
+  ${INPUT_FOLDER}/test_speed_gake ${N} > ${OUTPUT_FOLDER}/gake_${N}.txt
 done
