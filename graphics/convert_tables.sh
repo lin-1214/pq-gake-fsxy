6c6b796585885a3b6140a46898694af038f5231b
José Ignacio Escribano <jiep12345@hotmail.com>
2022-02-15 11:16:09 +0100

    Update results of gake test

--- graphics/convert_tables.sh
+++ graphics/convert_tables.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PARTIES_CONF="graphics/PARTIES.conf"
+KEM_CONF="graphics/KEM.conf"
 JSON2CSV_FOLDER="graphics"
 FOLDER="build"
 OUTPUT="results"
@@ -18,6 +19,8 @@ reverse() {
 IFS=$'\n' read -d '' -r -a Ns < ${PARTIES_CONF}
 reverse Ns Ns2
 
+IFS=$'\n' read -d '' -r -a Ks < ${KEM_CONF}
+
 TYPES=(
   "ake"
   "kem"
@@ -30,14 +33,17 @@ mkdir -p ${FOLDER}/${OUTPUT}
 for type in "${TYPES[@]}"
 do
   if [[ ${type} == "gake" ]]; then
-    for N in "${Ns2[@]}"
+    for kem in "${Ks[@]}"
     do
-      type_gake="${type}_${N}"
-      echo $type_gake
-      echo ${FOLDER}/${type}.txt
-      echo ${FOLDER}/${OUTPUT}/${type_gake}.csv
-      cat ${FOLDER}/${type_gake}.txt | sed '1,14d;$d' | ./node_modules/parse-markdown-table-cli/bin | node ${JSON2CSV_FOLDER}/json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type_gake}.csv
-      cp ${FOLDER}/${type_gake}.txt ${FOLDER}/${OUTPUT}
+      for N in "${Ns2[@]}"
+      do
+        type_gake="${type}_${kem}_${N}"
+        echo $type_gake
+        echo ${FOLDER}/${type}.txt
+        echo ${FOLDER}/${OUTPUT}/${type_gake}.csv
+        cat ${FOLDER}/${type_gake}.txt | sed '1,14d;$d' | ./node_modules/parse-markdown-table-cli/bin | node ${JSON2CSV_FOLDER}/json2csv.js ${type} ${N} > ${FOLDER}/${OUTPUT}/${type_gake}.csv
+        cp ${FOLDER}/${type_gake}.txt ${FOLDER}/${OUTPUT}
+      done
     done
   else
     echo $type
--- graphics/run_tests.sh
+++ graphics/run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PARTIES_CONF="graphics/PARTIES.conf"
+KEM_CONF="graphics/KEM.conf"
 INPUT_FOLDER="build/bin"
 OUTPUT_FOLDER="build"
 
@@ -17,6 +18,8 @@ reverse() {
 IFS=$'\n' read -d '' -r -a Ns < ${PARTIES_CONF}
 reverse Ns Ns2
 
+IFS=$'\n' read -d '' -r -a Ks < ${KEM_CONF}
+
 echo -e "Running KEM..."
 ${INPUT_FOLDER}/test_speed_kem > ${OUTPUT_FOLDER}/kem.txt
 
@@ -26,8 +29,13 @@ ${INPUT_FOLDER}/test_speed_ake > ${OUTPUT_FOLDER}/ake.txt
 echo -e "Running Commitment..."
 ${INPUT_FOLDER}/test_speed_commitment > ${OUTPUT_FOLDER}/commitment.txt
 
-for N in "${Ns2[@]}"
+echo -e "Running GAKE..."
+for kem in "${Ks[@]}"
 do
-  echo -e "Running GAKE-${N}..."
-  ${INPUT_FOLDER}/test_speed_gake ${N} > ${OUTPUT_FOLDER}/gake_${N}.txt
+  echo -e "Running kem ${kem}..."
+  for N in "${Ns2[@]}"
+  do
+    echo -e "\tRunning ${kem}-${N}..."
+    ${INPUT_FOLDER}/test_speed_gake ${N} ${kem} > ${OUTPUT_FOLDER}/gake_${kem}_${N}.txt
+  done
 done
